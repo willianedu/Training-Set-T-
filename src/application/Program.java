be6052fc52aa5d3184eb3edb3eb9392a88bcd779
willianedu <willianeduardo9877@hotmail.com>
2023-02-16 15:04:49 -0300

    Teste Exceptions

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,55 +1,43 @@
 package application;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.ImportedProduct;
-import entities.Product;
-import entities.UsedProduct;
+import model.entities.Account;
+import model.exceptions.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		List<Product> list = new ArrayList<>();
-		System.out.print("Enter the number of products:");
-		int n = sc.nextInt();
-		for (int i = 1; i <= n; i++) {
-			System.out.println("Product #" + i + " data:");
-			System.out.print("Common,used or imported (c/u/i)?");
-			char x = sc.next().charAt(0);
-			sc.nextLine();
-			System.out.print("Name:");
-			String name = sc.nextLine();
-			System.out.print("Price:");
-			Double price = sc.nextDouble();
-			if (x == 'i') {
-				System.out.print("Customs fee:");
-				Double customsFee = sc.nextDouble();
-				Product produto = new ImportedProduct(name, price, customsFee);
-				list.add(produto);
-			} else if (x == 'u') {
-				System.out.println("Manufacture date (DD/MM/YYYY):");
-				Date manufactureDate = sdf.parse(sc.next());
-				Product produto = new UsedProduct(name, price, manufactureDate);
-				list.add(produto);
-			} else {
-				Product produto = new Product(name, price);
-				list.add(produto);
-			}
+		try {
+		System.out.println("Enter account data");
+		System.out.print("Number:");
+		int numero = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Holder:");
+		String nome = sc.nextLine();
+		System.out.print("Initial balance:");
+		Double initialBalance = sc.nextDouble();
+		System.out.print("Withdraw limit:");
+		Double withdrawLimit = sc.nextDouble();
+		
+		Account conta = new Account(numero,nome,initialBalance,withdrawLimit);
+		
+		System.out.println("");
+		System.out.print("Enter amount for withdraw:");
+		Double withdraw = sc.nextDouble();
+		
+		conta.withdraw(withdraw);
+		System.out.println("New balance: " + conta.getBalance());
 		}
-		System.out.println();
-		System.out.println("PRICE TAGS:");
-		for (Product produto : list) {
-			System.out.println(produto.priceTag());
-			sc.close();
+		catch (DomainException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
 		}
+		catch (RuntimeException e) {
+			System.out.println("Unexpected error");
+		}
+		sc.close();
 	}
 }
